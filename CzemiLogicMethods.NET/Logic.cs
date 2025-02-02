b7d8bbeb20c5eb914a7d76b8d29eac5e0b338bda
Ssemakula Mukiibi <Ssemakula@users.noreply.github.com>
2025-02-02 12:59:42 +0300

    Add project files.

---
+++ CzemiLogicMethods.NET/Logic.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+namespace Czemi.LogicMethods
+{
+    /// <summary>
+    /// Implements C type logic where 0 or null is false else true
+    /// </summary>
+    public static class Logic
+    {
+        public static bool IsTrue(this int value) => value != 0;
+
+        public static bool IsTrue(this uint value) => value != 0;
+
+        public static bool IsTrue(this long value) => value != 0;
+
+        public static bool IsTrue(this ulong value) => value != 0;
+
+        public static bool IsTrue(this short value) => value != 0;
+
+        public static bool IsTrue(this ushort value) => value != 0;
+
+        public static bool IsTrue(this BigInteger value) => value != 0;
+
+        public static bool IsTrue(this byte value) => value != 0;
+
+        public static bool IsTrue(this sbyte value) => value != 0;
+
+        public static bool IsTrue(this float value) => value != 0;
+
+        public static bool IsTrue(this double value) => value != 0;
+
+        public static bool IsTrue(this decimal value) => value != 0;
+
+        public static bool IsTrue(this Enum value) => Convert.ToInt64(value) != 0;
+
+        public static bool IsTrue(this string value) => !string.IsNullOrEmpty(value);
+
+        public static bool IsTrue(this object value) => value != null;
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+## LogicMethods ##
+	<type>.Istrue(<expression>)
+	Returns true if expression is not 0 or empty
+
+
+ # Other Things #
+ Written using C# v 13 +
+ Code should be framework and platform independent
+ Code can be uses as it on all .NET models that support LINQ
+
+ Licensed under [MIT Licence](https://opensource.org/license/mit)
+ 
+  
